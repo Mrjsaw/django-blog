aeb1661af6a0f7122481bec3776d98df12f41d50
Imanuel <mrjsaw@gmail.com>
2021-01-24 13:14:38 +0100

    adding api landing layout

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,7 @@ from .models import Comment
 from django import forms
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(label='content', max_length = 500)
+    content = forms.CharField(label='content', max_length = 100)
     class Meta:
         model = Comment
         fields = ['content']
