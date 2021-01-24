e14bdde53bf5a2ffad529d4ee8f3a03bf36f5a48
Imanuel <mrjsaw@gmail.com>
2021-01-24 10:40:04 +0100

    debug false...

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -54,6 +54,7 @@ API_KEY_CUSTOM_HEADER = "HTTP_X_API_KEY"
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
+#test
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
